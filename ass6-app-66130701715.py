93b5f587e9d66b921d96c38241a3b17af61a5491
todsawatkmutt <161877245+todsawatkmutt@users.noreply.github.com>
2024-03-22 02:57:15 +0700

    Update ass6-app-66130701715.py

--- ass6-app-66130701715.py
+++ ass6-app-66130701715.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import pickle
-import numpy as np
 import streamlit as st
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
@@ -29,13 +27,10 @@ y_train = df['Label']
 
 text_clf_logreg.fit(X_train, y_train)
 
-# Evaluate the model
-logreg_train_accuracy = text_clf_logreg.score(X_train, y_train)
-st.write("Logistic Regression Training Accuracy:", logreg_train_accuracy)
 # Create a text input for testing the model
-test_text = st.text_input("ใส่ข้อความทดสอบ:", "")
+test_text = st.text_input("Enter a text for testing:", "")
 
 # Predict the label for the test text
-if st.button("ทดสอบโมเดล"):
+if st.button("Test Model"):
     prediction = text_clf_logreg.predict([test_text])
-    st.write("ผลการทดสอบ:", prediction)
+    st.write("Prediction:", prediction)
