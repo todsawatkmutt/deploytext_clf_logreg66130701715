c45b9fc216522ed81e6fbe0f1fce864d2e2830b7
todsawatkmutt <161877245+todsawatkmutt@users.noreply.github.com>
2024-03-22 01:23:12 +0700

    Add files via upload

---
+++ ass6-app-66130701715.py
@@ -0,0 +1,16 @@
+from sklearn.linear_model import LogisticRegression
+
+# สร้างและฝึกโมเดล Logistic Regression
+text_clf_logreg = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', LogisticRegression(max_iter=1000))
+])
+
+text_clf_logreg.fit(X_train, y_train)
+
+# ประเมินประสิทธิภาพของโมเดล
+logreg_train_accuracy = text_clf_logreg.score(X_train, y_train)
+logreg_test_accuracy = text_clf_logreg.score(X_test, y_test)
+print("Logistic Regression Training Accuracy:", logreg_train_accuracy)
+print("Logistic Regression Test Accuracy:", logreg_test_accuracy)
